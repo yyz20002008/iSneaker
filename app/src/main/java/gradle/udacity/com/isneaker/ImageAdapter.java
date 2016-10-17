fee63307c1eaef719140865c3caca8e2e53c0481
yyz20002008 <yyz20002008@gmail.com>
2016-10-16 22:08:14 -0400

    C. Cursor Query 1st

--- app/src/main/java/gradle/udacity/com/isneaker/ImageAdapter.java
+++ app/src/main/java/gradle/udacity/com/isneaker/ImageAdapter.java
@@ -2,7 +2,8 @@ package gradle.udacity.com.isneaker;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.provider.UserDictionary;
+import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import gradle.udacity.com.isneaker.data.SneakerDB;
+import gradle.udacity.com.isneaker.data.SneakerDBColumns;
 import gradle.udacity.com.isneaker.data.SneakerProvider;
 import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
 
@@ -25,7 +26,7 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
     private final int mHeight;
     private final int mWidth;
     private LayoutInflater inflater;
-    private String[] mCountries;
+    //private String[] mCountries;
 
     public ImageAdapter(Context c) {
 
@@ -33,7 +34,7 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
         mHeight= Math.round(mContext.getResources().getDimension(R.dimen.poster_height));
         mWidth = Math.round(mContext.getResources().getDimension(R.dimen.poster_width));
         inflater = LayoutInflater.from(c);
-        mCountries = c.getResources().getStringArray(R.array.countries);
+       // mCountries = c.getResources().getStringArray(R.array.countries);
     }
 
     public int getCount() {
@@ -50,6 +51,9 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
 
     public void insertSneaker(int image_id){
 
+        // Defines a new Uri object that receives the result of the insertion
+        Uri mNewUri;
+
         // Defines an object to contain the new values to insert
         ContentValues mNewValues = new ContentValues();
 
@@ -58,13 +62,15 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
          * method are "column name" and "value"
          */
 
-        mNewValues.put(SneakerProvider.Sneakers.model, "20161005");
-        mNewValues.put(SneakerDB.SNEAKERS.name, "Air_Jordan_1");
-        mNewValues.put(SneakerDB.SNEAKERS.release_date, "Oct,15 2016");
-        mNewValues.put(SneakerDB.SNEAKERS.release_time, "10:00AM");
+        mNewValues.put(SneakerDBColumns.MODEL, "20161005");
+        mNewValues.put(SneakerDBColumns.NAME, "Air_Jordan_1");
+        mNewValues.put(SneakerDBColumns.RELEASE_DATE, "Oct,15 2016");
+        mNewValues.put(SneakerDBColumns.RELEASE_TIME, "10:00AM");
+        mNewValues.put(SneakerDBColumns.IMAGE_URL, image_id);
+        mNewValues.put(SneakerDBColumns.ONLINE_STORE_LINK, "www.NIKE.com");
 
-        mNewUri = getContentResolver().insert(
-                SneakerDB.SNEAKERS.CONTENT_URI,   // the user dictionary content URI
+        mNewUri = mContext.getContentResolver().insert(
+                SneakerProvider.Sneakers.CONTENT_URI,   // content URI
                 mNewValues                          // the values to insert
         );
 
@@ -72,6 +78,7 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
     }
 
 
+    Cursor mCursor;
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
@@ -84,11 +91,53 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
         } else {
             imageView = (ImageView) convertView;
         }
+        //1. Insert Data
+        insertSneaker(mThumbIds[position]);
+        //imageView.setImageResource(mThumbIds[position]); //regular way to retrieve image
 
-        //imageView.setImageResource(mThumbIds[position]);
+        //2.Retrieve Data
+        // A "projection" defines the columns that will be returned for each row
+        String[] mProjection =
+                {
+                        SneakerDBColumns.MODEL,
+                        SneakerDBColumns.NAME,
+                        SneakerDBColumns.RELEASE_DATE,
+                        SneakerDBColumns.RELEASE_TIME,
+                        SneakerDBColumns.IMAGE_URL,
+                        SneakerDBColumns.ONLINE_STORE_LINK
+                };
 
-        Picasso.with(mContext).load(mThumbIds[position]).into(imageView);
+        // Defines a string to contain the selection clause
+                String mSelectionClause = null;
+
+        // Initializes an array to contain selection arguments
+                String[] mSelectionArgs = {""};
+
+        // Does a query against the table and returns a Cursor object
+                mCursor = mContext.getContentResolver().query(
+                        SneakerProvider.Sneakers.CONTENT_URI,
+                        mProjection,                       // The columns to return for each row
+                        mSelectionClause,                  // Either null, or the word the user entered
+                        mSelectionArgs,                    // Either empty, or the string the user entered
+                        SneakerDBColumns.RELEASE_DATE
+                );
+
+
+
+        // Determine the column index of the column named "word"
+        int index = mCursor.getColumnIndex(SneakerDBColumns.IMAGE_URL);
+
+        //Finally, add image to view
+        Picasso.with(mContext).load(Integer.parseInt(mCursor.getString(index))).into(imageView);//Picasso API
+
+        /*
+         * Only executes if the cursor is valid. The User Dictionary Provider returns null if
+         * an internal error occurs. Other providers may throw an Exception instead of returning null.
+         */
+
+
         return imageView;
+
      /*   ViewHolder holder;
 
         if (convertView == null) {
