5bacc93c0cc23bdb192e9c898d1894a93881a31b
yyz20002008 <yyz20002008@gmail.com>
2016-10-18 23:04:02 -0400

    C. Cursor Adapter

--- app/src/main/java/gradle/udacity/com/isneaker/ImageAdapter.java
+++ app/src/main/java/gradle/udacity/com/isneaker/ImageAdapter.java
@@ -1,34 +1,31 @@
 package gradle.udacity.com.isneaker;
 
-import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
-import gradle.udacity.com.isneaker.data.SneakerDBColumns;
-import gradle.udacity.com.isneaker.data.SneakerProvider;
 import se.emilsjolander.stickylistheaders.StickyListHeadersAdapter;
 
 /**
  * Created by James Yang on 10/7/2016.
  */
-public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapter {
+public class ImageAdapter extends CursorAdapter implements StickyListHeadersAdapter {
     private Context mContext;
     private final int mHeight;
     private final int mWidth;
     private LayoutInflater inflater;
     //private String[] mCountries;
 
-    public ImageAdapter(Context c) {
+    public ImageAdapter(Context c, Cursor cursor) {
+        super(c, cursor, 0);
 
         mContext = c;
         mHeight= Math.round(mContext.getResources().getDimension(R.dimen.poster_height));
@@ -49,36 +46,39 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
         return 0;
     }
 
-    public void insertSneaker(int image_id){
 
-        // Defines a new Uri object that receives the result of the insertion
-        Uri mNewUri;
 
-        // Defines an object to contain the new values to insert
-        ContentValues mNewValues = new ContentValues();
+    // The newView method is used to inflate a new view and return it,
+    // you don't bind any data to the view at this point.
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return inflater.inflate(R.layout.fragment_main, parent, false);
+    }
 
-        /*
-         * Sets the values of each column and inserts the word. The arguments to the "put"
-         * method are "column name" and "value"
-         */
+    // The bindView method is used to bind all data to a given view
+    // such as setting the text on a TextView.
+    @Override
+    public void bindView(View convertView, Context context, Cursor cursor) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(mWidth, mHeight));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        // Extract properties from cursor
 
-        mNewValues.put(SneakerDBColumns.MODEL, "20161005");
-        mNewValues.put(SneakerDBColumns.NAME, "Air_Jordan_1");
-        mNewValues.put(SneakerDBColumns.RELEASE_DATE, "Oct,15 2016");
-        mNewValues.put(SneakerDBColumns.RELEASE_TIME, "10:00AM");
-        mNewValues.put(SneakerDBColumns.IMAGE_URL, image_id);
-        mNewValues.put(SneakerDBColumns.ONLINE_STORE_LINK, "www.NIKE.com");
+        int priority = cursor.getInt(cursor.getColumnIndexOrThrow("image_url"));
+        //add image to view
+        // Populate fields with extracted properties
+        Picasso.with(mContext).load(priority).into(imageView);//Picasso API
 
-        mNewUri = mContext.getContentResolver().insert(
-                SneakerProvider.Sneakers.CONTENT_URI,   // content URI
-                mNewValues                          // the values to insert
-        );
 
 
     }
-
-
-    Cursor mCursor;
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent) {
         ImageView imageView;
@@ -91,12 +91,11 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
         } else {
             imageView = (ImageView) convertView;
         }
-        //1. Insert Data
-        insertSneaker(mThumbIds[position]);
+
         //imageView.setImageResource(mThumbIds[position]); //regular way to retrieve image
 
         //2.Retrieve Data
-        // A "projection" defines the columns that will be returned for each row
+      /*  // A "projection" defines the columns that will be returned for each row
         String[] mProjection =
                 {
                         SneakerDBColumns.MODEL,
@@ -125,33 +124,13 @@ public class ImageAdapter extends BaseAdapter  implements StickyListHeadersAdapt
 
 
         // Determine the column index of the column named "word"
-        int index = mCursor.getColumnIndex(SneakerDBColumns.IMAGE_URL);
+        int index = mCursor.getColumnIndex(SneakerDBColumns.IMAGE_URL);*/
 
         //Finally, add image to view
-        Picasso.with(mContext).load(Integer.parseInt(mCursor.getString(index))).into(imageView);//Picasso API
-
-        /*
-         * Only executes if the cursor is valid. The User Dictionary Provider returns null if
-         * an internal error occurs. Other providers may throw an Exception instead of returning null.
-         */
-
-
+        Picasso.with(mContext).load(mThumbIds[position]).into(imageView);//Picasso API
         return imageView;
 
-     /*   ViewHolder holder;
 
-        if (convertView == null) {
-            holder = new ViewHolder();
-            convertView = inflater.inflate(R.layout.test_list_item_layout, parent, false);
-            holder.text = (TextView) convertView.findViewById(R.id.text);
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
-        }
-
-        holder.text.setText(mCountries[position]);
-
-        return convertView;*/
     }
 
     @Override
--- app/src/main/java/gradle/udacity/com/isneaker/MainActivityFragment.java
+++ app/src/main/java/gradle/udacity/com/isneaker/MainActivityFragment.java
@@ -1,14 +1,23 @@
 package gradle.udacity.com.isneaker;
 
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.GridView;
 import android.widget.Toast;
 
+import gradle.udacity.com.isneaker.data.SneakerDBColumns;
+import gradle.udacity.com.isneaker.data.SneakerProvider;
 import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
 
 /**
@@ -16,32 +25,105 @@ import se.emilsjolander.stickylistheaders.StickyListHeadersListView;
  */
 public class MainActivityFragment extends Fragment {
 
+    private ImageAdapter mImages;
+    private Context mContext;
+    Cursor mCursor;
     public MainActivityFragment() {
     }
 
+    public void insertSneaker(int image_id){
 
-    GridView gridview;
-    private ImageAdapter mImages;
+        // Defines a new Uri object that receives the result of the insertion
+        Uri mNewUri;
+
+        // Defines an object to contain the new values to insert
+        ContentValues mNewValues = new ContentValues();
+
+        /*
+         * Sets the values of each column and inserts the word. The arguments to the "put"
+         * method are "column name" and "value"
+         */
+
+        mNewValues.put(SneakerDBColumns.MODEL, "20161005");
+        mNewValues.put(SneakerDBColumns.NAME, "Air_Jordan_1");
+        mNewValues.put(SneakerDBColumns.RELEASE_DATE, "Oct,15 2016");
+        mNewValues.put(SneakerDBColumns.RELEASE_TIME, "10:00AM");
+        mNewValues.put(SneakerDBColumns.IMAGE_URL, image_id);
+        mNewValues.put(SneakerDBColumns.ONLINE_STORE_LINK, "www.NIKE.com");
+
+        mNewUri = mContext.getContentResolver().insert(
+                SneakerProvider.Sneakers.CONTENT_URI,   // content URI
+                mNewValues                          // the values to insert
+        );
+
+
+    }
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.aj1sb,
+            R.drawable.aj12ovo, R.drawable.aj12wool,
+            R.drawable.aj31orange, R.drawable.aj4premium
+    };
+    // Defines the asynchronous callback for the contacts data loader
+    private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
+            new LoaderManager.LoaderCallbacks<Cursor>() {
+                // Create and return the actual cursor loader for the contacts data
+                @Override
+                public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+                    //1. Insert Data
+                    insertSneaker(mThumbIds[0]);
+                    // Define the columns to retrieve
+                    String[] projectionFields = new String[] {  SneakerDBColumns._ID,
+                            SneakerDBColumns.IMAGE_URL,};
+
+                    // Construct the loader
+                    CursorLoader cursorLoader = new CursorLoader(getActivity(),
+                            SneakerProvider.Sneakers.CONTENT_URI, // URI
+                            projectionFields, // projection fields
+                            null, // the selection criteria
+                            null, // the selection args
+                            SneakerDBColumns.RELEASE_DATE // the sort order
+                    );
+                    // Return the loader for use
+                    return cursorLoader;
+                }
+
+                // When the system finishes retrieving the Cursor through the CursorLoader,
+                // a call to the onLoadFinished() method takes place.
+                @Override
+                public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+                    // The swapCursor() method assigns the new Cursor to the adapter
+                    //mImages.swapCursor(cursor);
+                }
+
+                // This method is triggered when the loader is being reset
+                // and the loader data is no longer available. Called if the data
+                // in the provider changes and the Cursor becomes stale.
+                @Override
+                public void onLoaderReset(Loader<Cursor> loader) {
+                    // Clear the Cursor we were using with another call to the swapCursor()
+                    //mImages.swapCursor(null);
+                }
+            };
+
+    // Defines the id of the loader for later reference
+    public static final int CONTACT_LOADER_ID = 78; // From docs: A unique identifier for this loader. Can be whatever you want.
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fragment_main, container, false);
-        mImages=new ImageAdapter(getActivity());
-      /*  gridview = (GridView) rootView.findViewById(R.id.gridview);
-        gridview.setAdapter(mImages);
+        // Initialize the loader with a special ID and the defined callbacks from above
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Toast.makeText(getActivity(), "" + position,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-*/
+
+        mImages=new ImageAdapter(mContext,mCursor);
+
         StickyListHeadersListView stickyList = (StickyListHeadersListView) rootView.findViewById(R.id.list);
 
         stickyList.setAdapter(mImages);
-
+        getLoaderManager().initLoader(CONTACT_LOADER_ID, new Bundle(), contactsLoader);
         stickyList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
